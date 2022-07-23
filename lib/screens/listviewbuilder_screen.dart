0e34994dfa67c52d5c9bf2791876f66e0b2dd881
Alexander Mendoza <alexamp1987@gmail.com>
2022-07-23 12:01:34 -0500

    Card Flutter 2.2

--- lib/screens/listviewbuilder_screen.dart
+++ lib/screens/listviewbuilder_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class ListViewBuilderScreen extends StatefulWidget {
@@ -27,15 +28,21 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
 
   Future fetchData() async {
     if (isLoading) return;
-    isLoading = true;
+    isLoading = true; //Para mostrar de manera condicional el positioned
     setState(() {});
 
     Future.delayed(const Duration(seconds: 3));
     add5();
     isLoading = false;
-    setState(() {
-      
-    });
+    setState(() {});
+
+
+    if (scrollController.position.pixels + 100 <= scrollController.position.maxScrollExtent) return;
+    scrollController.animateTo(
+      scrollController.position.pixels + 120, 
+      duration: const Duration( milliseconds: 300 ), 
+      curve: Curves.fastOutSlowIn
+    );
   }
 
   void add5() {
@@ -44,34 +51,73 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     setState(() {});
   }
 
+  Future<void> onRefresh() async{
+    await Future.delayed(const Duration(seconds: 2));
+    final lastId = imageIds.last;
+    imageIds.clear();
+    imageIds.add(lastId + 1);
+    add5();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-        // appBar: AppBar(),
-        body: MediaQuery.removePadding(
+        // appBar: AppBar()
+      body: MediaQuery.removePadding(
       context: context,
       removeTop: true,
       removeBottom: true,
       child: Stack(
         children: [
-          ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            controller: scrollController,
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return FadeInImage(
-                  width: double.infinity,
-                  height: 300,
-                  fit: BoxFit.cover,
-                  placeholder: const AssetImage('assets/jar-loading.gif'),
-                  image: NetworkImage(
-                      'https://picsum.photos/id/237/200/300?image=${imageIds[index]}'));
-            },
+          RefreshIndicator(
+            color: AppTheme.primary,
+            onRefresh: onRefresh,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              controller: scrollController,
+              itemCount: imageIds.length,
+              itemBuilder: (BuildContext context, int index) {
+                return FadeInImage(
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                    placeholder: const AssetImage('assets/jar-loading.gif'),
+                    image: NetworkImage(
+                        'https://picsum.photos/500/300?image=${imageIds[index]}'));
+              },
+            ),
           ),
 
-          Container()
+          if( isLoading )
+          Positioned(
+              bottom: 40,
+              left: size.width * 0.5 - 30,
+              child: const _LoadingIcon()
+          )
         ],
       ),
     ));
+  }
+}
+
+class _LoadingIcon extends StatelessWidget {
+  const _LoadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.9), shape: BoxShape.circle),
+      child: const CircularProgressIndicator(
+        color: AppTheme.primary,
+      ),
+    );
   }
 }
